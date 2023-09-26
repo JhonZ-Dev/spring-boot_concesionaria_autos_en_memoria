ed1d5d8b4dbde7941b67b9406046057c828e9b82
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-25 22:33:25 -0500

    Update CitasModels.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/CitasModels.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/CitasModels.java
@@ -1,6 +1,10 @@
 package com.example.springbootconcesariatymleaf.model;
 
+import java.time.LocalDate;
+
 public class CitasModels {
 
-    
+    private Long id;
+    private LocalDate fecha_estimada;
+    private String hora_estimada;
 }
