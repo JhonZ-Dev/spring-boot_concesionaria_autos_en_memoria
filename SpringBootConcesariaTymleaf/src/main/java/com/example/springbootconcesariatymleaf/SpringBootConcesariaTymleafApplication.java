586363a1e5398deb70fde80be90d4caba27a3048
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-25 22:24:19 -0500

    Creación Poryecto

---
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/SpringBootConcesariaTymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootconcesariatymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConcesariaTymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConcesariaTymleafApplication.class, args);
+	}
+
+}
---
+++ SpringBootConcesariaTymleaf/src/test/java/com/example/springbootconcesariatymleaf/SpringBootConcesariaTymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootconcesariatymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConcesariaTymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
