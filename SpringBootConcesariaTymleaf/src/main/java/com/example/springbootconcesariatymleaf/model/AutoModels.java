6b6daeaa7320fb559e3b6457e6e8c99de53e6b27
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-25 22:29:57 -0500

    Update AutoModels.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/AutoModels.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/AutoModels.java
@@ -1,5 +1,10 @@
 package com.example.springbootconcesariatymleaf.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AutoModels {
     private Long id;
     private String modelo;
