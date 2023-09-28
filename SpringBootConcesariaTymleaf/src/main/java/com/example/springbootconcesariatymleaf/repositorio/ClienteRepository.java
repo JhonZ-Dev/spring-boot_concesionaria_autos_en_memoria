7d30af4c3dd4e8b5fb79e97fb2299fdef7c562a9
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-27 20:14:14 -0500

    Update ClienteRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
@@ -1,7 +1,12 @@
 package com.example.springbootconcesariatymleaf.repositorio;
 
+import com.example.springbootconcesariatymleaf.model.ClienteModel;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class ClienteRepository {
+    /*Una lista de map para almacenar los datos del cliente*/
+    private Map<Long, ClienteModel> clientes;
 }
