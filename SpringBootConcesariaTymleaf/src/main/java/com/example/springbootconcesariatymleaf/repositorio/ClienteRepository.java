a97cb0b5c0c762db68f6209a5a9148885104207e
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-27 20:14:48 -0500

    Update ClienteRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
@@ -4,9 +4,16 @@ import com.example.springbootconcesariatymleaf.model.ClienteModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class ClienteRepository {
     /*Una lista de map para almacenar los datos del cliente*/
     private Map<Long, ClienteModel> clientes;
+
+    /*Como es en memoria necesitamos un Id generado*/
+
+    private AtomicLong idGenerator;
+
+
 }
