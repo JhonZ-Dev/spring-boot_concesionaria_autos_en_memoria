8a0435b253584a70057df21d58ee29c341af60be
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-27 20:20:44 -0500

    Update ClienteRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
@@ -4,6 +4,7 @@ import com.example.springbootconcesariatymleaf.model.ClienteModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
