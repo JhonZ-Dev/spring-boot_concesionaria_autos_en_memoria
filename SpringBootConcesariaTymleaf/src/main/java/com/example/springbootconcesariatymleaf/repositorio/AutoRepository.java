c029516af0badca370d0aa20a18112f20240d7d3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-27 20:24:29 -0500

    Update AutoRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/AutoRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/AutoRepository.java
@@ -4,9 +4,11 @@ import com.example.springbootconcesariatymleaf.model.AutoModels;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class AutoRepository {
 
     private Map<Long, AutoModels> autos;
+    private AtomicLong idGenerator;
 }
