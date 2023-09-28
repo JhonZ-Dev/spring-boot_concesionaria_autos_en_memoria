225555314ea37bbce442322777959587f4698491
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-27 20:19:30 -0500

    Update ClienteRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/ClienteRepository.java
@@ -24,10 +24,23 @@ public class ClienteRepository {
 
     /*Guardar*/
     public ClienteModel saveCliente(ClienteModel cliente) {
-        long id = idGenerator.incrementAndGet();
-        cliente.setId(id);
-        clientes.put(id, cliente);
-        return cliente;
+        if (cliente.getId() != null) {
+            // El cliente ya tiene un ID asignado, por lo tanto, es una actualización
+            if (clientes.containsKey(cliente.getId())) {
+                // Reemplazar el cliente existente con los nuevos datos
+                clientes.put(cliente.getId(), cliente);
+                return cliente;
+            } else {
+                // Manejar el caso en el que el cliente con el ID proporcionado no existe
+                throw new IllegalArgumentException("Cliente no encontrado para el ID: " + cliente.getId());
+            }
+        } else {
+            // El cliente no tiene un ID asignado, por lo tanto, es un nuevo cliente a crear
+            long id = idGenerator.incrementAndGet();
+            cliente.setId(id);
+            clientes.put(id, cliente);
+            return cliente;
+        }
     }
 
 
