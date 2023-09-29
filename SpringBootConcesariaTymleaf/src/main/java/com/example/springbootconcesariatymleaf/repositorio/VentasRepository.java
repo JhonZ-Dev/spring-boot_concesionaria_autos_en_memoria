e19e33af1ae69b215f589a078cf13a846d60611a
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:49:19 -0500

    Update VentasRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/VentasRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/VentasRepository.java
@@ -15,4 +15,23 @@ public class VentasRepository {
         this.ventas = new HashMap<>();
         this.idGenerator = new AtomicLong(0);
     }
+
+
+    public VentasModels saveVenta(VentasModels venta) {
+        if (venta.getId() != null) {
+            // La venta ya tiene un ID asignado, por lo tanto, es una actualización
+            if (ventas.containsKey(venta.getId())) {
+                // Reemplazar la venta existente con los nuevos campos
+                ventas.put(venta.getId(), venta);
+                return venta;
+            } else {
+                throw new IllegalArgumentException("Venta no encontrada para el ID: " + venta.getId());
+            }
+        } else {
+            long id = idGenerator.incrementAndGet();
+            venta.setId(id);
+            ventas.put(id, venta);
+            return venta;
+        }
+    }
 }
