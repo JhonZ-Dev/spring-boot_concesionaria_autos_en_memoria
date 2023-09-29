e6e4d992891abad30dafe1379c7a93d5f4db00f5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:45:41 -0500

    Update CitasService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/CitasService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/CitasService.java
@@ -12,5 +12,22 @@ public class CitasService {
 
     private Map<Long, CitasModels> citas;
     private AtomicLong idGenerator;
-   
+
+    public CitasModels saveCita(CitasModels cita) {
+        if (cita.getId() != null) {
+            // La cita ya tiene un ID asignado, por lo tanto, es una actualización
+            if (citas.containsKey(cita.getId())) {
+                // Reemplazar la cita existente con los nuevos campos
+                citas.put(cita.getId(), cita);
+                return cita;
+            } else {
+                throw new IllegalArgumentException("Cita no encontrada para el ID: " + cita.getId());
+            }
+        } else {
+            long id = idGenerator.incrementAndGet();
+            cita.setId(id);
+            citas.put(id, cita);
+            return cita;
+        }
+    }
 }
