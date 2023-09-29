4e05860e0754018785857da9f197cdaa99db9231
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:51:07 -0500

    Update ClienteService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/ClienteService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/ClienteService.java
@@ -1,8 +1,10 @@
 package com.example.springbootconcesariatymleaf.servicio;
 
+import com.example.springbootconcesariatymleaf.repositorio.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
-    
+    private ClienteRepository clienteRepository;
+
 }
