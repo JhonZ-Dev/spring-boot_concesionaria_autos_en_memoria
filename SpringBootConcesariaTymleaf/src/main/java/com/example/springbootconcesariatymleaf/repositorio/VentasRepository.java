470034814d0fa98afddd9d8b64ca4f051b179169
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:47:19 -0500

    Update VentasRepository.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/VentasRepository.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/VentasRepository.java
@@ -2,6 +2,11 @@ package com.example.springbootconcesariatymleaf.repositorio;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Repository
 public class VentasRepository {
+    private Map<Long, VentasModels> ventas;
+    private AtomicLong idGenerator;
 }
