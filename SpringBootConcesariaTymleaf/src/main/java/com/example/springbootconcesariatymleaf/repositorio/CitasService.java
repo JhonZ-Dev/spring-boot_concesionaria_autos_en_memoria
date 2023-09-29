40c82865360966793d8afb270b3e2640e5206670
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:42:04 -0500

    Update CitasService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/CitasService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/repositorio/CitasService.java
@@ -1,9 +1,12 @@
 package com.example.springbootconcesariatymleaf.repositorio;
 
+import com.example.springbootconcesariatymleaf.model.CitasModels;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class CitasService {
 
-   
+    private Map<Long, CitasModels> citas;
 }
