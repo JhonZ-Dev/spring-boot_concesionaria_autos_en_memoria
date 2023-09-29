f3794400b1b4e6685c232ee4fe0ed526f5ab8959
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-28 20:48:09 -0500

    Update VentasModels.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/VentasModels.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/model/VentasModels.java
@@ -1,11 +1,16 @@
 package com.example.springbootconcesariatymleaf.model;
 
-import java.time.LocalDate;
+import lombok.Data;
 
+import java.time.LocalDate;
+@Data
 public class VentasModels {
 
     private Long id;
     private LocalDate fecha_venta;
     private double precio;
     private AutoModels auto;
+
+
+    private ClienteModel cliente;
 }
