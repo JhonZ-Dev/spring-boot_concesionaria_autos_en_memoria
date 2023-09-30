eb743a9dd754b91292e9065557456bdb2ba2914f
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-29 22:05:57 -0500

    Update AutoService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/AutoService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/AutoService.java
@@ -1,5 +1,6 @@
 package com.example.springbootconcesariatymleaf.servicio;
 
+import com.example.springbootconcesariatymleaf.model.AutoModels;
 import com.example.springbootconcesariatymleaf.repositorio.AutoRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,8 @@ public class AutoService {
     private AutoRepository autoRepository;
     public AutoService() {
         this.autoRepository = new AutoRepository();
+    }
+    public AutoModels saveAuto(AutoModels auto) {
+        return autoRepository.saveAuto(auto);
     }
 }
