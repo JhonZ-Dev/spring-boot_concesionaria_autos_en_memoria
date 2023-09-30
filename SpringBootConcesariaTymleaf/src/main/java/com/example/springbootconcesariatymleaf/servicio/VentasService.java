938d298acf242059d0bd51d7e8160e08cf227889
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-29 22:11:20 -0500

    Update VentasService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/VentasService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/VentasService.java
@@ -4,6 +4,8 @@ import com.example.springbootconcesariatymleaf.model.VentasModels;
 import com.example.springbootconcesariatymleaf.repositorio.VentasRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class VentasService {
     private VentasRepository ventasRepository;
@@ -15,6 +17,10 @@ public class VentasService {
     }
     public VentasModels getVentaById(Long id) {
         return ventasRepository.getVentaById(id);
+    }
+
+    public List<VentasModels> getAllVentas() {
+        return ventasRepository.getAllVentas();
     }
 
 }
