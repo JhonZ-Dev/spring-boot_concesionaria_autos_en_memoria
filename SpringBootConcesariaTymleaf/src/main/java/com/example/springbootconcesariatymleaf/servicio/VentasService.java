6598a235de543f8eceeb8d23931667cdb0973bad
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-29 22:10:29 -0500

    Update VentasService.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/VentasService.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/servicio/VentasService.java
@@ -1,5 +1,6 @@
 package com.example.springbootconcesariatymleaf.servicio;
 
+import com.example.springbootconcesariatymleaf.repositorio.VentasRepository;
 import org.springframework.stereotype.Service;
 
 @Service
