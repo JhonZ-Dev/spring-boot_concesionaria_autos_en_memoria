01bf4c85dcb6b7e478857e6f80346b528b6e116b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-30 21:22:00 -0500

    Update ClienteController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/ClienteController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.example.springbootconcesariatymleaf.controller;
 
 import com.example.springbootconcesariatymleaf.servicio.ClienteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +16,11 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    /*Metodo para guardar un cliente*/
+    @GetMapping("/nuevo")
+    public String mostrarFormularioNuevoCliente(Model model) {
+        model.addAttribute("cliente", new ClienteModel());
+        return "cliente_formulario";
+    }
 
 }
