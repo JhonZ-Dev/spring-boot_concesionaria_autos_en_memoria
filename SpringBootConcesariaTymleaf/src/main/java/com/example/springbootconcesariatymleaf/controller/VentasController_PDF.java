18beb03c932b55d9a83969d154ca918254f5e77b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-30 21:34:32 -0500

    Update VentasController_PDF.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/VentasController_PDF.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/VentasController_PDF.java
@@ -1,12 +1,21 @@
 package com.example.springbootconcesariatymleaf.controller;
 
 import com.example.springbootconcesariatymleaf.servicio.VentasService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.IOException;
 
 @Controller
 
 public class VentasController_PDF {
     @Autowired
     private VentasService ventasService;
+
+    public void generarPDF(@PathVariable("id") Long ventaId, HttpServletResponse response) throws IOException {
+
+        
+    }
 }
