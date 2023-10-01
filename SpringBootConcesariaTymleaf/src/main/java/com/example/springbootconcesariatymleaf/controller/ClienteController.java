bcfbb76b9bb29630907afb08fc6c95e736776b7c
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-30 21:21:02 -0500

    Update ClienteController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/ClienteController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/ClienteController.java
@@ -1,9 +1,13 @@
 package com.example.springbootconcesariatymleaf.controller;
 
+import com.example.springbootconcesariatymleaf.servicio.ClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
+
+    private ClienteService clienteService;
+
 }
