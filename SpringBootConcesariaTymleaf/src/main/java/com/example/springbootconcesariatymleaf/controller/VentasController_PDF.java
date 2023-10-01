07233ee1c860102a4a8e35c185120c843eba555c
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-30 21:33:10 -0500

    Update VentasController_PDF.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/VentasController_PDF.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/VentasController_PDF.java
@@ -1,8 +1,12 @@
 package com.example.springbootconcesariatymleaf.controller;
 
+import com.example.springbootconcesariatymleaf.servicio.VentasService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 
 public class VentasController_PDF {
+    @Autowired
+    private VentasService ventasServ
 }
