de6edbf51894271c5c25687fe7e9a9a0d505bae4
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-01 22:22:54 -0500

    Update HomeController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/HomeController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.springbootconcesariatymleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
 }
