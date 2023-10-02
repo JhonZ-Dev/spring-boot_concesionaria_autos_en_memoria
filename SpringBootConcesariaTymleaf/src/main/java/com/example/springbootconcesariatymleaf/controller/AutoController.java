5a3041e6db2a0bdb18a4b73c3ca617dd5be541ee
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-01 22:19:13 -0500

    Create AutoController.java

---
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/AutoController.java
@@ -0,0 +1,7 @@
+package com.example.springbootconcesariatymleaf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/autos")
+public class AutoController {
+}
