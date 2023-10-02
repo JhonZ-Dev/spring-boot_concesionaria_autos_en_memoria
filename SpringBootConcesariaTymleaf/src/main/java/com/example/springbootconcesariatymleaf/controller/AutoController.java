b3171313dbfc70c109820cba6ed505da9f416613
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-01 22:20:03 -0500

    Update AutoController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/AutoController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/AutoController.java
@@ -1,6 +1,7 @@
 package com.example.springbootconcesariatymleaf.controller;
 
 import com.example.springbootconcesariatymleaf.servicio.AutoService;
+import com.example.springbootconcesariatymleaf.servicio.ClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
