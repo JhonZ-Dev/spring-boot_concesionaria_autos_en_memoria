5d352eedca5c5fe627f27a79ac501b8757576e90
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-06 22:32:30 -0500

    Update CitasController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController.java
@@ -3,7 +3,10 @@ package com.example.springbootconcesariatymleaf.controller;
 import com.example.springbootconcesariatymleaf.servicio.CitasService;
 import com.example.springbootconcesariatymleaf.servicio.ClienteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/citas")
@@ -15,5 +18,16 @@ public class CitasController {
     public CitasController(CitasService citasService, ClienteService clienteService) {
         this.citasService = citasService;
         this.clienteService = clienteService;
+    }
+
+    @GetMapping("/nueva")
+    public String mostrarFormularioNuevaCita(@RequestParam("clienteId") Long clienteId, Model model) {
+        ClienteModel cliente = clienteService.getClienteById(clienteId);
+        if (cliente == null) {
+            return "redirect:/clientes";
+        }
+        model.addAttribute("cliente", cliente);
+        model.addAttribute("cita", new CitasModels());
+        return "formulario-cita";
     }
 }
