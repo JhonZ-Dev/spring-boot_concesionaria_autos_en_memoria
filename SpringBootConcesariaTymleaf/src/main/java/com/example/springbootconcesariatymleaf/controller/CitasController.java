fb175e60ce630b3d78821b0d9b0f599820b70c0c
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-06 22:31:57 -0500

    Update CitasController.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController.java
@@ -1,9 +1,12 @@
 package com.example.springbootconcesariatymleaf.controller;
 
+import com.example.springbootconcesariatymleaf.servicio.CitasService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/citas")
 public class CitasController {
+
+    private final CitasService citasService;
 }
