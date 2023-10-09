d0b8f975fa9bfa5bbe33db4e1027ebc5b4c23057
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-08 21:38:57 -0500

    Update CitasController_PDF.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController_PDF.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController_PDF.java
@@ -1,7 +1,10 @@
 package com.example.springbootconcesariatymleaf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CitasController_PDF {
+    @Autowired
+    private CitasService citasService;
 }
