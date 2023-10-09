79ce3aa6b411059723e635a56c87a0ca39126536
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-08 21:41:29 -0500

    Update CitasController_PDF.java

--- SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController_PDF.java
+++ SpringBootConcesariaTymleaf/src/main/java/com/example/springbootconcesariatymleaf/controller/CitasController_PDF.java
@@ -1,10 +1,14 @@
 package com.example.springbootconcesariatymleaf.controller;
 
 import com.example.springbootconcesariatymleaf.servicio.CitasService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
 
 @Controller
 public class CitasController_PDF {
@@ -12,5 +16,8 @@ public class CitasController_PDF {
     private CitasService citasService;
 
     @RequestMapping(value = "/cita/{id}/pdf", method = RequestMethod.GET)
-    
+    public void generarPDF(@PathVariable("id")Long citaId,
+                           HttpServletResponse response) throws IOException{
+        
+    }
 }
